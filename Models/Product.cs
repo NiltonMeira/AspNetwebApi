ac14d68a25feb059c25485ac6dfd71d333246b8a
Nilton Meira <niltonprudys@gmail.com>
2024-10-31 10:43:28 -0300

    feat: creating models and installing packages

---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+namespace AspNetwebApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string? ImageURL { get; set; }
+        public float Stock { get; set; }
+        public DateTime RegisterDate { get; set; }
+    }
+}
