697410e0839566d38db9e9a8ab6a1037f11478b7
Nilton Meira <niltonprudys@gmail.com>
2024-10-31 10:58:24 -0300

    feat: AppDBContext

---
+++ Context/AppDBContext.cs
@@ -0,0 +1,16 @@
+using AspNetwebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetwebApi.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base (options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
